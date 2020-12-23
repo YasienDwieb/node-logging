1efab8e7de7ed26c77af0470b69e99113ee2389e
YasienDwieb <yasien.dwieb@gmail.com>
2020-12-23 15:20:36 +0200

    add timestamp to log

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,13 @@
-import { createLogger, transports } from 'winston';
+import { createLogger, format, transports } from 'winston';
+const { combine, timestamp, json } = format;
 
 const systemInfoLogger = createLogger({
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    json()
+  ),
   transports: [
     new transports.Console(),
     new transports.File({ filename: "systemInfo.log" })
@@ -8,9 +15,15 @@ const systemInfoLogger = createLogger({
 });
 
 const actionsLogger = createLogger({
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    json()
+  ),
   transports: [
     new transports.File({ filename: "actions.log" })
-  ]
+  ],
 });
 
 export { systemInfoLogger, actionsLogger };
